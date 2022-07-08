7ad07946327046651e155bad6abe3f4be52846d6
kobit <1848738+arturhefczyc@users.noreply.github.com>
2022-07-08 14:59:04 -0700

    Added support for different ZONE ID for domain and hostname

--- scripts/aws-update-zone.sh
+++ scripts/aws-update-zone.sh
@@ -3,21 +3,15 @@
 # Script updates records of a DNS ZONE on AWS Route53
 #
 # $1 - mandatory domain zone ID on AWS Route53
-# $2 - mandatory email server, like mail.example.com, that is address to which
-#      email app connects over IMAP to retrieve and send emails.
-# $3 - mandatory email server IP address.
-# $4 - optional email domain, like example.com
-#      that is address with email accounts like user@example.com
-# $5 - optional email domain IP address. If the email domain is used to connect
-#      to the server, like for webmail or anything it needs IP address as well
+# $2 - mandatory hostname, like mail.example.com
+# $3 - mandatory server IP address.
+# $4 - optional AWS profile
 
 
 ZONE_ID="${1}"
-EMAIL_SERVER="${2}"
-SERVER_IP="${3}"
-
-EMAIL_DOMAIN="${4}"
-DOMAIN_IP="${5}"
+HOSTNAME="${2}"
+IP="${3}"
+PROFILE="${4}"
 
 cat <<EOF > ~/tmp-zone.json
 {
@@ -28,40 +22,22 @@ cat <<EOF > ~/tmp-zone.json
             {
                 "Action": "UPSERT",
                 "ResourceRecordSet": {
-                    "Name": "${EMAIL_SERVER}",
+                    "Name": "${HOSTNAME}",
                     "Type": "A",
                     "TTL": 300,
                     "ResourceRecords": [
                         {
-                            "Value": "${SERVER_IP}"
+                            "Value": "${IP}"
                         }
                     ]
                 }
             }
-EOF
-
-[ -z "${EMAIL_DOMAIN}" ] || cat <<EOF >> ~/tmp-zone.json
-,
-            {
-                "Action": "UPSERT",
-                "ResourceRecordSet": {
-                    "Name": "${EMAIL_DOMAIN}",
-                    "Type": "A",
-                    "TTL": 300,
-                    "ResourceRecords": [
-                        {
-                            "Value": "${DOMAIN_IP}"
-                        }
-                    ]
-                }
-            }
-EOF
-
-cat <<EOF >> ~/tmp-zone.json
         ]
     }
 }
 EOF
 
-aws route53 change-resource-record-sets --cli-input-json file://~/tmp-zone.json
+[[ -n "${PROFILE}" ]] && export AWS_PROFILE=${PROFILE}
+
+aws route53 change-resource-record-sets --cli-input-json file://~/tmp-zone.json 
 
